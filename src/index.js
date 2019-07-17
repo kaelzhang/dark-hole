299857c73db6b295ace4c35c5c558630b9173c57
Kael Zhang <i+github@kael.me>
2019-07-17 16:26:45 +0800

    optimize

--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
-// istanbul ignore next
-if (typeof Proxy === 'undefined') {
-  // istanbul ignore next
-  throw new Error('dark-hole requires Proxy')
-}
-
 module.exports = () => {
   let proxy
-
   const origin = () => proxy
-  proxy = new Proxy(origin, {
+  return proxy = new Proxy(origin, {
     // Accepts everything
-    get () {
-      // And always returns itself
-      return proxy
-    }
+    // And always returns itself
+    get: origin
   })
-
-  return proxy
 }
