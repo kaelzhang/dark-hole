8a0b99989c667ca968078b7cb3eb07c6daa5bc82
Kael Zhang <i+github@kael.me>
2019-09-22 13:53:17 +0800

    WIP

--- src/error.js
+++ src/error.js
@@ -4,7 +4,8 @@ const {E, TE, error} = new Errors()
 
 E('STACK_NOT_FOUND', '')
 
-TE('INVALID_ACCESSOR', 'accessor should be a string of an array of strings')
+TE('INVALID_ACCESSOR',
+  'accessor should be a string, an array of strings or undefined')
 
 module.exports = {
   error
--- src/trace.js
+++ src/trace.js
@@ -1,10 +1,10 @@
 const {
   isArray, isString
 } = require('core-util-is')
-
 const {error} = require('./error')
 
 const STACK = Symbol('dark-hole:stack')
+const LENGTH = Symbol('dark-hole:length')
 const ACCESS = Symbol('dark-hole:access')
 const CALL = Symbol('dark-hole:call')
 
@@ -32,9 +32,17 @@ const MATCHER = {
   },
 
   FunctionCall (target, expect) {
-
+    return target.thisObject === expect.this.Object
+    && arrayEqual(target.args, expect.args)
   }
 }
+
+const matchTrace = (target, expect) =>
+  target.type !== expect.type
+  && MATCHER[expect.type](target, expect)
+
+const matchTraces = (traces, expect, start) =>
+  expect.every((ex, i) => matchTrace(traces[start + i], ex))
 
 const parseAccessor = accessor => {
   if (accessor === undefined) {
@@ -58,66 +66,76 @@ const createTracesByAccessor = accessor =>
 class Tracer {
   constructor (stack) {
     this[STACK] = stack
+    this[LENGTH] = stack.length
   }
 
-  _create (trace) {
+  // Extend new trace to the tracer
+  _extend (trace) {
     return new Tracer(this[STACK].concat(trace))
   }
 
   [ACCESS] (property) {
-    return this._create(propTrace(property))
+    return this._extend(propTrace(property))
   }
 
   [CALL] (args, thisObject) {
-    return this._create(callTrace(args, thisObject))
+    return this._extend(callTrace(args, thisObject))
   }
 
   get [Symbol.iterator] () {
     return this[STACK][Symbol.iterator]
   }
 
-  _next (ofIndex) {
-    return new Tracer(this[STACK].slice(ofIndex + 1))
-  }
-
   get done () {
     return this[STACK].length === 0
   }
 
-  _isMatchTraces (traces, start, lookAhead) {
+  _matchTraces (expect, start, lookAhead) {
+    const {length} = expect
 
+    if (start + length > this[LENGTH]) {
+      return - 1
+    }
+
+    const matched = matchTraces(this[STACK], expect, start)
+
+    if (matched || !lookAhead) {
+      return matched
+        ? start
+        : - 1
+    }
+
+    return this._matchTraces(expect, start + 1, true)
   }
 
-  _willBeCalledWith (options) {
+  _match (expect, begin) {
+    const start = this._matchTraces(expect, 0, !begin)
 
+    if (start === - 1) {
+      throw error('NOT_MATCH')
+    }
+
+    return new Tracer(this[STACK].slice(start + expect.length))
   }
 
   willBeCalledWith ({
     accessor,
-    args,
+    args = [],
     thisObject,
     begin = true
   }) {
-    if (accessor) {
-      return this.willBeAccessedBy({
-        accessor,
-        begin
-      })
-      .willBeCalledWith({
-        args,
-        thisObject,
-        begin: true
-      })
-    }
+    const expect = createTracesByAccessor(accessor)
+    .concat(callTrace(args, thisObject))
 
-    const current = this[STACK][0]
+    return this._match(expect, begin)
   }
 
   willBeAccessedBy ({
     accessor,
     begin = true
   }) {
-
+    const expect = createTracesByAccessor(accessor)
+    return this._match(expect, begin)
   }
 }
 
